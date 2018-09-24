2270505df8b98ff0abac73d89801bed621b88058
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-09-24 22:58:37 +0700

    ramdisk: raph: Inline
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- ramdisk/init.raphiel.sh
+++ ramdisk/init.raphiel.sh
@@ -30,16 +30,16 @@ function delett {
 # devfreq tuning
 # Qualcomm is using obscure directory for devfreq, so follow up
 restorecon -R /sys/class/devfreq/*qcom,cpubw*
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/governor cpufreq
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/sample_ms 4
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/io_percent 34
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/hist_memory 20
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/hyst_length 10
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/low_power_ceil_mbps 0
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/low_power_io_percent 34
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/low_power_delay 20
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/guard_band_mbps 0
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/up_scale 250
-get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/idle_mbps 1600
+# FIME : We're now forcing Performance governor, drop this for now
+# get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/sample_ms 4
+# get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/io_percent 34
+# get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/hist_memory 20
+# get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/hyst_length 10
+# get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/low_power_ceil_mbps 0
+# get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/low_power_io_percent 34
+# get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/low_power_delay 20
+# get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/guard_band_mbps 0
+# get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/up_scale 250
+# get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/idle_mbps 1600
 get-set-forall /sys/class/devfreq/*qcom,mincpubw*/governor cpufreq
 }&
